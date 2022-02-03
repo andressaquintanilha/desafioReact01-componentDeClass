ede26da32ebf45dd5341e74a01a4dc13e5ad29fb
Andressa Quintanilha <78860791+andressaquintanilha@users.noreply.github.com>
2022-02-03 20:35:13 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    //localização - 'so abre a gaveta'
+    return (
+      //retorna algo não especifico, ele mostra 'a gaveta toda'
+      <div>
+        <p>Estou achando dificil!</p>
+      </div>
+    );
+  }
+}
+
+export default App; //o que o console faz (especificamente dentro da 'gaveta' do return)
